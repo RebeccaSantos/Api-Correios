37e5fd73f0d4da171e36c60219f8264d9cca9e64
rebecca <revip.pop@gmail.com>
2020-11-18 11:34:30 -0300

    arrumando status 500

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,POST');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     app.use(cors());
     next();
 });
