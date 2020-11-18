ede950d59169390466a9e3b4833215fce9497cab
rebecca <revip.pop@gmail.com>
2020-11-18 19:56:53 -0300

    tomara que de certo

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express()
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
