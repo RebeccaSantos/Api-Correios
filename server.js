ad19a88494e06b093150d94ae8ad109f80763ee6
rebecca <revip.pop@gmail.com>
2020-11-17 21:24:55 -0300

    adicionando permissão para o frontend

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", 'GET,POST');
+    app.use(cors());
+    next();
+});
 
 require('./src')(app)
 
