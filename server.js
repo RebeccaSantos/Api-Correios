15e2cefc3aa3d2b85e53ca0f234fabdae99efe2b
rebecca <revip.pop@gmail.com>
2020-11-18 22:49:28 -0300

    aaaaa

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", 'GET,POST');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
     app.use(cors());
     next();
 });
